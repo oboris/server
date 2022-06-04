94bc0ed0ae9679724767eecc4ab926b075e1968b
Boris Onyshchenko <oboris@ukr.net>
2022-06-04 22:42:17 +0300

    new test coding

--- app/routes/init_routes.js
+++ app/routes/init_routes.js
@@ -1,6 +1,27 @@
 module.exports = function(app, db) {
+  app.get('/inits/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': db.ObjectId(id) };
+    db.collection('init').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        console.log(item);
+        res.send(item);
+      } 
+    });
+  });
+
 	app.post('/inits', (req, res) => {
-    
-    res.send('Hello')
+    const item = { name: req.body.name, title: req.body.title };
+    db.collection('init').insertOne(item, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result);
+        console.log(result);
+        console.log(init);
+      }
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 const app            = express();
 
 const port = 8000;
 
-require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+MongoClient.connect(db.url, (err, client) => {
+  if (err) return console.log(err)
+  const db = client.db('test')
+  require('./app/routes')(app, db);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
 });
